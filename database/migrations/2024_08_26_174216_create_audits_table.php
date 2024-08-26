cb65d9798893cbdd2a4db5ac7db7607d792a2e47
Renzo Miranda <renzo.miranda@teamspan.com>
2024-08-26 18:00:28 +0000

    added fields to fillable method

--- database/migrations/2024_08_26_174216_create_audits_table.php
+++ database/migrations/2024_08_26_174216_create_audits_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('audits', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('lob')->nullable();
             $table->timestamps();
         });
     }
