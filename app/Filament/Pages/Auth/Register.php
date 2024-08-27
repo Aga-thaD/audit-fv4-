bfd16b6134a65a4c8df9ee2952213af164af326a
Renzo Miranda <renzo.miranda@teamspan.com>
2024-08-27 20:36:16 +0000

    added default permissions for associates after registering

--- app/Filament/Pages/Auth/Register.php
+++ app/Filament/Pages/Auth/Register.php
@@ -4,9 +4,12 @@ namespace App\Filament\Pages\Auth;
 
 use App\Models\User;
 use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Http\Responses\Auth\RegistrationResponse;
 use Filament\Pages\Page;
 use Filament\Pages\Auth\Register as BaseRegister;
+use Illuminate\Support\Facades\Auth;
 
 class Register extends BaseRegister
 {
@@ -44,5 +47,36 @@ class Register extends BaseRegister
     {
         return Hidden::make('user_role')
             ->default('Associate');
+    }
+
+    public function register(): ?RegistrationResponse
+    {
+        $data = $this->form->getState();
+
+        $user = $this->getUserModel()::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+            'user_role' => $data['user_role'],
+            'user_lob' => $data['user_lob'],
+            // Set permissions for Associate role
+            'audit_create' => false,
+            'audit_view' => true,
+            'audit_update' => true,
+            'audit_delete' => false,
+            'pqc_create' => false,
+            'pqc_view' => true,
+            'pqc_update' => true,
+            'pqc_delete' => false,
+            'user_create' => false,
+            'user_view' => false,
+            'user_update' => false,
+            'user_delete' => false,
+            // All other permissions default to false
+        ]);
+
+        Auth::login($user);
+
+        return app(RegistrationResponse::class);
     }
 }
