e3919a121914a607db8a5b89f0d2e2edaf3bbf0c
Renzo Miranda <renzo.miranda@teamspan.com>
2024-08-27 17:16:42 +0000

    added 2nd row of stats overview widgets

---
+++ app/Filament/Widgets/StatsOverview2ndRow.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Audit;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview2ndRow extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Pending Audits', Audit::where('aud_status', 'Pending')->count())
+                ->icon('heroicon-o-sparkles'),
+            Stat::make('Disputed Audits', Audit::where('aud_status', 'Disputed')->count())
+                ->icon('heroicon-o-x-circle'),
+            Stat::make('Acknowledged Audits', Audit::where('aud_status', 'Acknowledged')->count())
+                ->icon('heroicon-o-check-circle'),
+        ];
+    }
+}
