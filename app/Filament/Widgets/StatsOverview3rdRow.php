6c02470b46bccc0a30264a2b7b52e9f401515adc
Renzo Miranda <renzo.miranda@teamspan.com>
2024-08-27 18:14:38 +0000

    added 3rd row stats overview widget

---
+++ app/Filament/Widgets/StatsOverview3rdRow.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\PhoneQC;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview3rdRow extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Pending Phone QCs', PhoneQC::where('pqc_status', 'Pending')->count())
+                ->icon('heroicon-o-sparkles'),
+            Stat::make('Disputed Phone QCs', PhoneQC::where('pqc_status', 'Disputed')->count())
+                ->icon('heroicon-o-x-circle'),
+            Stat::make('Acknowledged Phone QCs', PhoneQC::where('pqc_status', 'Acknowledged')->count())
+                ->icon('heroicon-o-check-circle'),
+        ];
+    }
+}
