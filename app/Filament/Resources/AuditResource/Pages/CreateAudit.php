161f5e3f68b2756dab6a3f2bf82a4d0128ae27dd
agatha <agatha.deladia@teamspan.com>
2025-03-26 19:52:42 +0000

    added email response to failed audits

--- app/Filament/Resources/AuditResource/Pages/CreateAudit.php
+++ app/Filament/Resources/AuditResource/Pages/CreateAudit.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\AuditResource\Pages;
 
 use App\Filament\Resources\AuditResource;
+use App\Mail\AuditMail;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Mail;
 
 class CreateAudit extends CreateRecord
 {
@@ -14,4 +18,36 @@ class CreateAudit extends CreateRecord
     {
         return $this->getResource()::getUrl('index');
     }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+
+        if($this->record->lob === "CINTAS ACCOUNTS RECEIVABLE") { 
+            if($this->record->pass_fail === "Fail") 
+            {
+                
+                $manager_email = auth()->user()->email;
+                $ar_info = $this->record->user_id;
+                $ar_email = User::find($ar_info);
+
+                $body = "Audit with EO Number - " . $this->record->eo_number . " by " . $ar_email->name . "'s is tagged as FAILED.";
+                $title = "Audit Fail - " . $this->record->eo_number;
+
+            Mail::to($manager_email)
+                ->send(new AuditMail($title, $body));
+
+            Mail::to($ar_email->email)
+                ->send(new AuditMail($title, $body));
+            }
+        }
+   
+
+    return Notification::make()
+            ->title('Audit Created!')
+            ->success()
+            ->send();
+
+    }
+
 }
+
---
+++ app/Mail/AuditMail.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class AuditMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $title, $body;
+
+    public function __construct($title, $body)
+    {
+        $this->title = $title;
+        $this->body = $body;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->title,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.auditmail',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
