a8ffd678670c72c8488e23e233de9db143250e18
Aaron Marquez <aaron.marquez@teamspan.com>
2025-03-27 19:39:31 +0000

    added full width for table widgets

--- app/Filament/Widgets/RecentPhoneQCs.php
+++ app/Filament/Widgets/RecentPhoneQCs.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 
 class RecentPhoneQCs extends BaseWidget
 {
+    protected int | string | array $columnSpan = 'full';
 
     public static function canView(): bool
     {
